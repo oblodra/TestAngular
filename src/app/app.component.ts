58940947b9271297f090c6d9198d9ee4de527fd9
Bengt Petersson <btgpetersson@gmail.com>
2017-01-10 15:03:29 +0100

    Adding first components for basic Hero Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  `,
 //  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
+  
 })
 export class AppComponent {
-  name = 'Angular';
-// title = 'app works!';
+  name = 'Tour of Heroes';
+  title = 'app works!';
+  hero: Hero = {
+  id: 1,
+  name: 'Windstorm'
+};
 }
+
+
